003b0bb584bb2274a13f3793a1bf4acf31b39714
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-11 22:03:05 -0500

    modified constructor

--- src/main/java/com/banking/system/CheckingAccount.java
+++ src/main/java/com/banking/system/CheckingAccount.java
@@ -1,10 +1,9 @@
 package com.banking.system;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public class CheckingAccount extends Account{
-    public CheckingAccount(int accountNum, String accountHolder, int balance, LocalDate dateAccountOpened, List<Transaction> transactions) {
-        super(accountNum, accountHolder, balance, dateAccountOpened, transactions);
+    public CheckingAccount(int accountNum, String accountHolder, int balance, LocalDate dateAccountOpened) {
+        super(accountNum, accountHolder, balance, dateAccountOpened);
     }
 }
