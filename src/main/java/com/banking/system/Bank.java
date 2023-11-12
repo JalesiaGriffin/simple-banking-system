956f40ee0c2223485e38a9cfeb8f946c53f060dd
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-11 22:04:06 -0500

    created generate new user id method

--- src/main/java/com/banking/system/Bank.java
+++ src/main/java/com/banking/system/Bank.java
@@ -1,15 +1,50 @@
 package com.banking.system;
 
-public class Bank {
-    public void addCustomer(){
+import java.util.List;
 
+public class Bank {
+    private List<Customer> customers;
+    private List<Integer> userIds;
+
+    public void addCustomer(Customer customer){
+        customers.add(customer);
     }
 
-    public void openAccount(){
-
+    public void openAccount(Customer customer, Account account){
+        customer.addAccount(account);
     }
 
     public void displayCustomerInfo(){
+        for(Customer customer : customers){
+            // Personal
+            System.out.println("************************************************");
+            System.out.println(customer.getName() + "'s Personal Information");
+            System.out.println("User ID: " + customer.getBankUserId());
+            System.out.println("Phone: " + customer.getPhoneNum());
+            System.out.println("Email: " + customer.getEmail());
+            System.out.println("Address: " + customer.getAddress());
 
+            // Banking
+            System.out.println("Banking Information");
+            for (Account account: customer.getAccounts()) {
+                System.out.println("Account Number: " + account.getAccountNum());
+                System.out.println("Balance: $" + account.getBalance());
+                if (account instanceof SavingsAccount savings) {
+                    System.out.println("Interest Rate: " + savings.getInterestRate());
+                }
+            }
+            System.out.println("************************************************");
+        }
+    }
+
+    public int generateNewUserId(){
+        int userId = (int)(Math.random() * 90000) + 10000;
+        for(int id : userIds){
+            if (id == userId){
+                userId = (int)(Math.random() * 90000) + 10000;
+            }
+        }
+        userIds.add(userId);
+    return userId;
     }
 }
